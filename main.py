93ed705d8732e2744a77e2d0f0a3abf86137bab6
anhaddahat <anhaddahat2@gmail.com>
2023-12-20 21:55:18 +0530

    i have imported the necessary libraries: NEXT:  create a datamodel

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+import secrets
+
+app = FastAPI()
+
+@app.post()
