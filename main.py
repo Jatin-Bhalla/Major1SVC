fa1a90e09aef9f8bfa1851dc9e094fb9389499ea
Jatin-Bhalla <jain@remote.com>
2023-12-21 01:41:51 +0530

    the data model was updated; please add the necessary functions for the jwt process and jwt bearer

--- main.py
+++ main.py
@@ -5,7 +5,38 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 import secrets
+#SECRET_KEY=secrets.token_hex(32)
+SCERET_KEY = "151d9e98ad3748ae85d00e2535964619e33a7003da07c869214d312858995734"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+db = {
+    "tim":{
+        "username": "tim",
+        "full_name": "Tim Ruscica",
+        "email": "tim@gmail.com",
+        "hashed_pass": "$2b$12$26QcftVZwe439mv7F18nMuBV/ZemWwN17OtZ0P4kS1ZSXD2.UDwOm", #hash from the name
+        "disabled": False
+    }
+}
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str or None = None
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+
+class UserInDB(User):
+    hashed_pass: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
 @app.post()
