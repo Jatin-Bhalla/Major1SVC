9e3ffeb762278350dcd35f8ddf56505379552acf
anhaddahat <anhaddahat2@gmail.com>
2023-12-21 09:40:30 +0530

    added the endpoints, check the code

--- main.py
+++ main.py
@@ -5,7 +5,108 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 import secrets
+#SECRET_KEY=secrets.token_hex(32)
+SCERET_KEY = "151d9e98ad3748ae85d00e2535964619e33a7003da07c869214d312858995734"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+db = {
+    "tim":{
+        "username": "tim",
+        "full_name": "Tim Ruscica",
+        "email": "tim@gmail.com",
+        "hashed_pass": "$2b$12$26QcftVZwe439mv7F18nMuBV/ZemWwN17OtZ0P4kS1ZSXD2.UDwOm", #hash from the name
+        "disabled": False
+    }
+}
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str or None = None
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+
+class UserInDB(User):
+    hashed_pass: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
+def verify_pass(plain_pass, hashed_pass):
+    return pwd_context.verify(plain_pass, hashed_pass)
 
-@app.post()
+def get_pass_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
+    
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_pass(password, user.hashed_pass):
+        return False
+    
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes = 1)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SCERET_KEY, algorithm = ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth_2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        payload = jwt.decode(token, SCERET_KEY, algorithms=[ALGORITHM])
+        username: str= payload.get("sub")
+        if username is None:
+            raise credential_exception
+        
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise credential_exception
+    
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+    
+    return user
+
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    
+    return current_user
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", header={"WWW-Authenticate": "Bearer"})
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_user_me(current_user: User = Depends(get_current_active_user)):
+                       return current_user
+@app.get("/users/me/items")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+     return [{"item_id": 1, "owner": current_user}]
